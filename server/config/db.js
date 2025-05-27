04e1801aca27a896b2c120c44e707d43041cf72d
BVChamarthi <bharathvarma169@gmail.com>
2025-05-27 12:21:54 +0300

    connected to MongoDB, added posts, created pages for posts

---
+++ server/config/db.js
@@ -0,0 +1,37 @@
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://bharathvarma169:y2PXJ8KHJx6x0ASp@cluster0.ox77kay.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+// async function connectDB() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+
+const mongoose = require('mongoose');
+const connectDB = async () => {
+  
+  try {
+    mongoose.set('strictQuery', false);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`Database Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
+
+}
+
+module.exports = { connectDB }
